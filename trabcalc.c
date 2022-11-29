46e160c01c364309f5be288a60661aa54c8a248c
Luca Rodrigues <luca12rodrigues@gmail.com>
2022-11-28 21:02:49 -0300

    fix: melhorias para o código

--- trabcalc.c
+++ trabcalc.c
@@ -2,27 +2,41 @@
 
 int main(){
 	
-	float N, M, R;
+	float N, M;
+	float R = 0;
 	int op;
 	
-	do{
-		do{
-			printf("Escolha uma operacao\n\n1.Soma\n2.Subtracao\n3.Multiplicao\n4.Divisao\n5.Somatorio\n6.Sair\n");
+	do {
+		do {
+			printf("Escolha uma operacao: \n");
+			printf("1. Soma\n");
+			printf("2. Subtracao\n");
+			printf("3. Multiplicao\n");
+			printf("4. Divisao\n");
+			printf("5. Somatorio\n");
+			printf("6. Sair\n");
+
 			scanf("%d", &op);
-			if(op < 1 || op > 6)
-				printf("O numero inserido precisa ser de 1 a 6.\n");
-		}while(op < 1 || op > 6);
-		if(op <= 4){
-			printf("\nDigite um valor N: ");
-			scanf("%f", &N);
-			printf("Digite um valor M: ");
+
+			if (op < 1 || op > 6) {
+				printf("\nO numero inserido precisa ser de 1 a 6.\n\n");
+			}
+
+		} while(op < 1 || op > 6);
+
+		if (op == 6) {
+			continue;
+		}
+
+		printf("\nDigite um valor N: ");
+		scanf("%f", &N);
+
+		if (op != 5) {
+			printf("\nDigite um valor M: ");
 			scanf("%f", &M);
 		}
-		else if(op == 5){
-			printf("\nDigite um valor N: ");
-			scanf("%f", &N);
-		}
-		switch(op){
+
+		switch (op) {
 			case 1:
 				R = N + M;
 				break;
@@ -33,17 +47,23 @@ int main(){
 				R = N * M;
 				break;
 			case 4:
+				if (M == 0) {
+					printf("\nImpossivel fazer divisao por zero\n\n");
+					R = 0;
+					continue;
+				}
+
 				R = N / M;
 				break;
 			case 5:
-				for(int i=0; i<= N; i++)
-					R = R + i;
+				for(int i = 0; i <=  N; i++) {
+					R += i;
+				}
 				break;
-		}			
-		if(op <= 5 )
-			printf("\nResultado: %.3f\n\n", R);
-		
+		}
+
+		printf("\nResultado: %.3f\n\n", R);
 		R = 0;
 		
-	}while(op != 6);
+	} while (op != 6);
 }
